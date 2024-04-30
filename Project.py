e25bfc165200a648dadb74d6b8bf1008eb969caa
Shashwat Abrol <shashwat2004.abrol@outlook.com>
2024-04-30 19:46:22 +0800

    Setting up flask

--- Project.py
+++ Project.py
@@ -1,1 +1,5 @@
-Flask
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ app/init.py
@@ -0,0 +1,7 @@
+"""Initializing the Flask application. """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+from app import route
